71525c257b2e4eee1defd42e3656640febec44c6
azamatislomov777-blip <azamatislomov777@gmail.com>
2026-06-24 11:23:10 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
+import os
+import time
+import requests
 
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+def send_message(text):
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+    requests.post(
+        url,
+        json={
+            "chat_id": CHAT_ID,
+            "text": text
+        }
+    )
+
+print("🚀 Bot started")
+
+send_message("✅ RSI Bot успешно запущен на Railway!")
+
+while True:
+    print("Bot is running...")
+    time.sleep(1200)  # 20 минут
